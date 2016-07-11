4f79c6c667454032eaa18aef5894eb9ba99cb120
lina <nwlf53@gmail.com>
2016-07-11 00:59:33 -0500

    Project Structure

--- src/main/java/edu/mum/waa/procurement/service/UserService.java
+++ src/main/java/edu/mum/waa/procurement/service/UserService.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.mum.waa.procurement.domain;
+package edu.mum.waa.procurement.service;
 
 /**
  *
- * @author 985042
+ * @author yonatangetachew
  */
-public class User {
+public class UserService {
     
 }
